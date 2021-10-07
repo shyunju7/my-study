ea84a96d6bedc1d1cb9fdacc18cda4952630035b
shyunju7 <shyunju7@naver.com>
2021-10-07 21:36:36 +0900

    Study useTabs

--- my-blog/src/211006/hooks/customHooks.js
+++ my-blog/src/211006/hooks/customHooks.js
@@ -1,16 +1,35 @@
-import { React, useState } from "react";
+import { useState } from "react";
 
-export const useInput = (initValue) => {
+export const useInput = (initValue, validator) => {
   const [value, setValue] = useState(initValue);
 
   const onChange = (e) => {
     const {
       target: { value },
     } = e;
-    console.log(value);
+
+    let willUpdate = false;
+
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+
+      console.log(`isValidated:`, willUpdate);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
+
     setValue(value);
   };
   return { value, onChange };
 };
 
-export const useTabs = () => {};
+export const useTabs = (initTabIndex, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initTabIndex);
+
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
--- my-blog/src/211006/index.js
+++ my-blog/src/211006/index.js
@@ -1,11 +1,31 @@
 import { React, useState } from "react";
-import { useInput } from "./hooks/customHooks";
+import { useInput, useTabs } from "./hooks/customHooks";
 const HooksStudy = () => {
   const [count, setCount] = useState(0);
-  const name = useInput("우든");
 
+  const checkEmail = (value) => {
+    const regExp =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (value.match(regExp)) return true;
+    else return false;
+  };
+
+  const email = useInput("wooden@woo.co.kr", checkEmail);
   const incrementCount = () => setCount(count + 1);
   const decrementCount = () => setCount(count - 1);
+
+  const allTabs = [
+    {
+      tab: "우든",
+      contents: "안녕하세요! 우든입니다.",
+    },
+    {
+      tab: "수봉",
+      contents: "안녕하세요! 수봉입니다.",
+    },
+  ];
+
+  const { currentItem, changeItem } = useTabs(0, allTabs);
 
   return (
     <div>
@@ -14,7 +34,12 @@ const HooksStudy = () => {
       <button onClick={decrementCount}> down </button>
 
       <h1> useInput</h1>
-      <input placeholder="name" {...name} />
+      <input placeholder="name" {...email} />
+
+      <h1>useTabs</h1>
+      <button onClick={() => changeItem(0)}>우든</button>
+      <button onClick={() => changeItem(1)}>수봉</button>
+      <p>{currentItem.contents}</p>
     </div>
   );
 };
