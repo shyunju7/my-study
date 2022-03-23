c25e7452e9350e0e5062d641e61b3ac895908ca6
shyunju7 <shyunju7@naver.com>
2022-03-23 18:13:49 +0900

    Install styled-component, react-router-dom, react-icons

--- my-study/src/App.js
+++ my-study/src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
-import HooksStudy from "./211009";
-
+import DelegationTest from "./eventDelegation/index";
+import styled from "styled-components";
+import { FaRegArrowAltCircleUp } from "react-icons/fa";
 const App = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    // document.querySelector("#home").scrollIntoView({
+    //   behavior: "smooth",
+    // });
+  };
   return (
-    <div>
-      <HooksStudy />
-    </div>
+    <Container>
+      <Button size="36px" color="#767676" onClick={handleScrollToTop} />
+      <DelegationTest />
+      <footer>@copyright 2022</footer>
+    </Container>
   );
 };
 
 export default App;
+const Container = styled.div`
+  position: relative;
+`;
+const Button = styled(FaRegArrowAltCircleUp)`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 1000;
+  outline: none;
+`;
